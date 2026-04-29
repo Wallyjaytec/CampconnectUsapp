f211e71e63095c824bec5fb8bd7838023d7fbaf1
Wally Jay Tech <wallyjay111@gmail.com>
2026-04-29 21:56:56 +0100

    Delete lib/modules/product directory

--- lib/modules/product/controller/description_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class DescriptionController extends GetxController {
-  final RxBool expanded = false.obs;
-  final RxBool needsMore = false.obs;
-  void toggle() => expanded.value = !expanded.value;
-}
