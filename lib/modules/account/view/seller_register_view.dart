81fd9c1df54a4ae86a11a71989d409bf8f02a32e
Wally Jay Tech <wallyjay111@gmail.com>
2026-04-30 23:36:49 +0100

    Refactor SellerRegisterView to use StatefulWidget

--- lib/modules/account/view/seller_register_view.dart
+++ lib/modules/account/view/seller_register_view.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class SellerRegisterView extends StatelessWidget {
+class SellerRegisterView extends StatefulWidget {
   const SellerRegisterView({super.key});
+
+  @override
+  State<SellerRegisterView> createState() => _SellerRegisterViewState();
+}
+
+class _SellerRegisterViewState extends State<SellerRegisterView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://campconnectus.store/seller/register'));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +25,7 @@ class SellerRegisterView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Become a Seller'),
       ),
-      body: WebView(
-        initialUrl: 'https://campconnectus.store/seller/register',
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
