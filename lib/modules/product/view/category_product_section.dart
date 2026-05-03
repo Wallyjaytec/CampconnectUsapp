0f1a78619849be865e045a6c4edab97fc17fb0dc
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-03 02:16:23 +0100

    Clean up imports in category_product_section.dart
    Removed unused imports from category_product_section.dart

--- lib/modules/product/view/category_product_section.dart
+++ lib/modules/product/view/category_product_section.dart
@@ -1,5 +1,3 @@
-import 'package:kartly_e_commerce/modules/product/controller/new_product_list_controller.dart';
-import 'package:kartly_e_commerce/data/repositories/product_repository.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
