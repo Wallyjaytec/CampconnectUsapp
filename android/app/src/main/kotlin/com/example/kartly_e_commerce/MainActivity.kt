9f129ab3d932a982cdc93200073fc98c8ae52e61
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-07 00:21:16 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/kartly_e_commerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kartly_e_commerce/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.example.kartly_e_commerce
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        val data = intent?.data
+        if (data != null) {
+            // Pass the URI to Flutter
+            intent.putExtra("deep_link_uri", data.toString())
+        }
+    }
+}
