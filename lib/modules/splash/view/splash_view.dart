a61c549ebc0187811b6b76fdda58914a4c34370a
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-07 00:36:32 +0100

    Enhance splash screen with deep link handling
    Updated splash screen logic to handle deep links for email verification and password reset.

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -1,20 +1,20 @@
 import 'dart:async';
-
+import 'package:get_storage/get_storage.dart';
 import 'package:kartly_e_commerce/core/constants/app_assets.dart';
 import 'package:kartly_e_commerce/core/constants/app_colors.dart';
 import 'package:kartly_e_commerce/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../auth/view/password_reset_view.dart';
+import '../../auth/view/verification_success_view.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
-
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _slideAnimation;
   late Animation<double> _fadeAnimation;
@@ -22,56 +22,32 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-
-    _slideAnimation = Tween<double>(begin: -300.0, end: 0.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeOutCubic,
-      ),
-    );
-
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.0, 0.3, curve: Curves.easeIn),
-      ),
-    );
-
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _slideAnimation = Tween<double>(begin: -300.0, end: 0.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _controller, curve: const Interval(0.0, 0.3, curve: Curves.easeIn)));
     _controller.forward();
 
     Timer(const Duration(seconds: 3), () {
       if (!mounted) return;
-
-      final rawUri = Uri.base.toString();
-
-      Get.dialog(
-        AlertDialog(
-          title: const Text('DEBUG'),
-          content: Text('URI: $rawUri'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Get.back();
-                Get.offAllNamed(AppRoutes.bottomNavbarView);
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
+      final box = GetStorage();
+      final token = box.read<String>('deep_link_token') ?? '';
+      final type = box.read<String>('deep_link_type') ?? '';
+      if (token.isNotEmpty) {
+        box.remove('deep_link_token');
+        box.remove('deep_link_type');
+        if (type == 'email_verify') {
+          Get.offAll(() => VerificationSuccessView(code: token));
+        } else {
+          Get.offAll(() => PasswordResetView(token: token));
+        }
+        return;
+      }
+      Get.offAllNamed(AppRoutes.bottomNavbarView);
     });
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  void dispose() { _controller.dispose(); super.dispose(); }
 
   @override
   Widget build(BuildContext context) {
@@ -80,23 +56,8 @@ class _SplashScreenState extends State<SplashScreen>
       body: Center(
         child: AnimatedBuilder(
           animation: _controller,
-          builder: (context, child) {
-            return Transform.translate(
-              offset: Offset(_slideAnimation.value, 0),
-              child: Opacity(
-                opacity: _fadeAnimation.value,
-                child: child,
-              ),
-            );
-          },
-          child: SizedBox(
-            width: 160,
-            height: 160,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: Image.asset(AppAssets.appLogo, fit: BoxFit.contain),
-            ),
-          ),
+          builder: (context, child) => Transform.translate(offset: Offset(_slideAnimation.value, 0), child: Opacity(opacity: _fadeAnimation.value, child: child)),
+          child: SizedBox(width: 160, height: 160, child: ClipRRect(borderRadius: BorderRadius.circular(24), child: Image.asset(AppAssets.appLogo, fit: BoxFit.contain))),
         ),
       ),
     );
