34af9b8e6c273e1f52c026b174a42debd116aa42
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-09 06:20:30 +0100

    Update product_details_controller.dart

--- lib/modules/product/controller/product_details_controller.dart
+++ lib/modules/product/controller/product_details_controller.dart
@@ -275,10 +275,7 @@ class ProductDetailsController extends GetxController {
       );
     }).toList();
 
-    final tag = 'add-to-cart-${p.id}';
-    if (Get.isRegistered<AddToCartController>(tag: tag)) {
-      Get.delete<AddToCartController>(tag: tag, force: true);
-    }
+    final tag = 'add-to-cart-${p.id}-${DateTime.now().millisecondsSinceEpoch}';
 
     final cartUi = CartUiProduct(id: p.id, title: safeName, imageUrl: img, price: safePrice, rating: safeRating);
 
