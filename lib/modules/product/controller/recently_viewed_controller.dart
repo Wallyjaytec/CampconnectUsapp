9d8dc1a7536840434faaa0dfcc8d4ee2b2341eb0
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-09 23:27:09 +0100

    Create recently_viewed_controller.dart

---
+++ lib/modules/product/controller/recently_viewed_controller.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import '../model/product_model.dart';
+
+class RecentlyViewedController extends GetxController {
+  final box = GetStorage();
+  static const String _key = 'recently_viewed_products';
+  static const int maxProducts = 20;
+
+  final RxList<ProductModel> products = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadFromStorage();
+  }
+
+  void loadFromStorage() {
+    final raw = box.read<List>(_key) ?? [];
+    final list = <ProductModel>[];
+    for (final item in raw) {
+      if (item is Map) {
+        try {
+          list.add(ProductModel.fromJson(Map<String, dynamic>.from(item)));
+        } catch (_) {}
+      }
+    }
+    products.assignAll(list);
+  }
+
+  void addProduct(ProductModel product) {
+    // Remove if already exists
+    products.removeWhere((p) => p.id == product.id);
+    // Add to front
+    products.insert(0, product);
+    // Keep only max
+    if (products.length > maxProducts) {
+      products.removeRange(maxProducts, products.length);
+    }
+    products.refresh();
+    _saveToStorage();
+  }
+
+  void _saveToStorage() {
+    final jsonList = products.map((p) => p.toJson()).toList();
+    box.write(_key, jsonList);
+  }
+
+  void clearAll() {
+    products.clear();
+    box.remove(_key);
+  }
+}
