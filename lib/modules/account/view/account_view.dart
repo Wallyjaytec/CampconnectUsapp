bb4aa7959166ea57cae7553c81d5dae0ae2fd0a2
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-10 18:46:40 +0100

    Update account_view.dart

--- lib/modules/account/view/account_view.dart
+++ lib/modules/account/view/account_view.dart
@@ -1,4 +1,4 @@
-here import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
