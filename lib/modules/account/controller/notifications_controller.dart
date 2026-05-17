e4ca6dc9db38c6bad4375343c8a0991a3353032d
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-17 18:15:43 +0100

    Update notifications_controller.dart

--- lib/modules/account/controller/notifications_controller.dart
+++ lib/modules/account/controller/notifications_controller.dart
@@ -20,6 +20,10 @@ class NotificationController extends GetxController {
 
   final items = <NotificationItem>[].obs;
   final notificationCount = 0.obs;
+  
+  // Selection mode
+  final isSelectionMode = false.obs;
+  final selectedCount = 0.obs;
 
   @override
   void onInit() {
@@ -122,5 +126,89 @@ class NotificationController extends GetxController {
       );
     }
     return success;
+  }
+
+  // Selection mode methods
+  void toggleSelection(NotificationItem item) {
+    item.isSelected = !item.isSelected;
+    items.refresh();
+    selectedCount.value = items.where((e) => e.isSelected).length;
+    isSelectionMode.value = selectedCount.value > 0;
+  }
+
+  void selectAll() {
+    for (var item in items) {
+      item.isSelected = true;
+    }
+    items.refresh();
+    selectedCount.value = items.length;
+    isSelectionMode.value = true;
+  }
+
+  void clearSelection() {
+    for (var item in items) {
+      item.isSelected = false;
+    }
+    items.refresh();
+    selectedCount.value = 0;
+    isSelectionMode.value = false;
+  }
+
+  Future<void> deleteSelected() async {
+    final selectedIds = items.where((e) => e.isSelected).map((e) => e.id).toList();
+    for (var id in selectedIds) {
+      await _repo.deleteNotification(id);
+    }
+    items.removeWhere((e) => e.isSelected);
+    clearSelection();
+    notificationCount.value = items.where((e) => !e.isRead).length;
+    Get.snackbar(
+      'Deleted'.tr,
+      '${selectedIds.length} notifications deleted'.tr,
+      backgroundColor: AppColors.primaryColor,
+      snackPosition: SnackPosition.TOP,
+      colorText: AppColors.whiteColor,
+    );
+  }
+
+  Future<void> markSelectedAsRead() async {
+    final selectedIds = items.where((e) => e.isSelected).map((e) => e.id).toList();
+    for (var id in selectedIds) {
+      await _repo.markSingleAsRead(notificationId: id);
+    }
+    for (var item in items) {
+      if (item.isSelected) {
+        item.isRead = true;
+      }
+    }
+    items.refresh();
+    clearSelection();
+    notificationCount.value = items.where((e) => !e.isRead).length;
+    Get.snackbar(
+      'Success'.tr,
+      '${selectedIds.length} notifications marked as read'.tr,
+      backgroundColor: AppColors.primaryColor,
+      snackPosition: SnackPosition.TOP,
+      colorText: AppColors.whiteColor,
+    );
+  }
+
+  Future<void> markSelectedAsUnread() async {
+    final selectedIds = items.where((e) => e.isSelected).map((e) => e.id).toList();
+    for (var item in items) {
+      if (item.isSelected) {
+        item.isRead = false;
+      }
+    }
+    items.refresh();
+    clearSelection();
+    notificationCount.value = items.where((e) => !e.isRead).length;
+    Get.snackbar(
+      'Success'.tr,
+      '${selectedIds.length} notifications marked as unread'.tr,
+      backgroundColor: AppColors.primaryColor,
+      snackPosition: SnackPosition.TOP,
+      colorText: AppColors.whiteColor,
+    );
   }
 }
