3115c67e354ea220e5e694a72b6827ba81ade98e
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-18 18:11:06 +0100

    Update splash_view.dart

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -22,51 +22,32 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   @override
   void initState() {
     super.initState();
-    
-    try {
-      _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-      _slideAnimation = Tween<double>(begin: -300.0, end: 0.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
-      _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _controller, curve: const Interval(0.0, 0.3, curve: Curves.easeIn)));
-      _controller.forward();
-    } catch (e) {
-      Get.snackbar('Animation Error', e.toString(), backgroundColor: Colors.red);
-    }
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _slideAnimation = Tween<double>(begin: -300.0, end: 0.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _controller, curve: const Interval(0.0, 0.3, curve: Curves.easeIn)));
+    _controller.forward();
 
     Timer(const Duration(seconds: 3), () {
-      try {
-        if (!mounted) return;
-        final box = GetStorage();
-        final token = box.read<String>('deep_link_token') ?? '';
-        final type = box.read<String>('deep_link_type') ?? '';
-        
-        if (token.isNotEmpty) {
-          box.remove('deep_link_token');
-          box.remove('deep_link_type');
-          if (type == 'email_verify') {
-            Get.offAll(() => VerificationSuccessView(code: token));
-          } else {
-            Get.offAll(() => PasswordResetView(token: token));
-          }
-          return;
+      if (!mounted) return;
+      final box = GetStorage();
+      final token = box.read<String>('deep_link_token') ?? '';
+      final type = box.read<String>('deep_link_type') ?? '';
+      if (token.isNotEmpty) {
+        box.remove('deep_link_token');
+        box.remove('deep_link_type');
+        if (type == 'email_verify') {
+          Get.offAll(() => VerificationSuccessView(code: token));
+        } else {
+          Get.offAll(() => PasswordResetView(token: token));
         }
-        Get.offAllNamed(AppRoutes.bottomNavbarView);
-      } catch (e) {
-        Get.snackbar('Navigation Error', e.toString(), backgroundColor: Colors.red);
-        // Fallback navigation
-        Future.delayed(Duration(seconds: 1), () {
-          Get.offAllNamed(AppRoutes.bottomNavbarView);
-        });
+        return;
       }
+      Get.offAllNamed(AppRoutes.bottomNavbarView);
     });
   }
 
   @override
-  void dispose() { 
-    try {
-      _controller.dispose();
-    } catch (e) {}
-    super.dispose(); 
-  }
+  void dispose() { _controller.dispose(); super.dispose(); }
 
   @override
   Widget build(BuildContext context) {
@@ -75,14 +56,8 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
       body: Center(
         child: AnimatedBuilder(
           animation: _controller,
-          builder: (context, child) => Transform.translate(
-            offset: Offset(_slideAnimation?.value ?? 0, 0), 
-            child: Opacity(opacity: _fadeAnimation?.value ?? 1, child: child),
-          ),
-          child: SizedBox(width: 160, height: 160, child: ClipRRect(
-            borderRadius: BorderRadius.circular(24), 
-            child: Image.asset(AppAssets.appLogo, fit: BoxFit.contain),
-          )),
+          builder: (context, child) => Transform.translate(offset: Offset(_slideAnimation.value, 0), child: Opacity(opacity: _fadeAnimation.value, child: child)),
+          child: SizedBox(width: 160, height: 160, child: ClipRRect(borderRadius: BorderRadius.circular(24), child: Image.asset(AppAssets.appLogo, fit: BoxFit.contain))),
         ),
       ),
     );
