54aa551f3c0247681e29ac8bc451822ec71a9826
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-18 14:41:34 +0100

    Refactor splash screen navigation logic

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../auth/view/password_reset_view.dart';
 import '../../auth/view/verification_success_view.dart';
-import '../../../core/utils/internet_checker.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,47 +18,27 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _checkAndNavigate();
-  }
-
-  Future<void> _checkAndNavigate() async {
-    await Future.delayed(const Duration(seconds: 2));
-    
-    final hasInternet = await InternetChecker().checkInternet();
-    
-    if (!hasInternet) {
-      // Show dialog and wait for internet
-      InternetChecker().startMonitoring();
-      
-      // Check every 2 seconds until internet comes back
-      Timer.periodic(const Duration(seconds: 2), (timer) async {
-        final connected = await InternetChecker().checkInternet();
-        if (connected && mounted) {
-          timer.cancel();
-          _navigateToNext();
-        }
-      });
-    } else {
-      _navigateToNext();
-    }
+    _navigateToNext();
   }
 
   void _navigateToNext() {
-    if (!mounted) return;
-    final box = GetStorage();
-    final token = box.read<String>('deep_link_token') ?? '';
-    final type = box.read<String>('deep_link_type') ?? '';
-    if (token.isNotEmpty) {
-      box.remove('deep_link_token');
-      box.remove('deep_link_type');
-      if (type == 'email_verify') {
-        Get.offAll(() => VerificationSuccessView(code: token));
-      } else {
-        Get.offAll(() => PasswordResetView(token: token));
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      final box = GetStorage();
+      final token = box.read<String>('deep_link_token') ?? '';
+      final type = box.read<String>('deep_link_type') ?? '';
+      if (token.isNotEmpty) {
+        box.remove('deep_link_token');
+        box.remove('deep_link_type');
+        if (type == 'email_verify') {
+          Get.offAll(() => VerificationSuccessView(code: token));
+        } else {
+          Get.offAll(() => PasswordResetView(token: token));
+        }
+        return;
       }
-      return;
-    }
-    Get.offAllNamed(AppRoutes.bottomNavbarView);
+      Get.offAllNamed(AppRoutes.bottomNavbarView);
+    });
   }
 
   @override
