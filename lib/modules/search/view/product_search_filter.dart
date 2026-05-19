96a9a18c86d34dcbb50ee95bbfdfaa40b23b6a7f
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-19 14:21:59 +0100

    Update product_search_filter.dart

--- lib/modules/search/view/product_search_filter.dart
+++ lib/modules/search/view/product_search_filter.dart
@@ -191,6 +191,7 @@ class ProductSearchFilter extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         appBar: AppBar(
           automaticallyImplyLeading: false,
           leadingWidth: 20,
