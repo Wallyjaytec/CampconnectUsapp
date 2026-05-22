57f555577923f26e5b5afb13f288261ce18ad823
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-22 19:15:22 +0100

    Create latest_collection_controller.dart

---
+++ lib/modules/product/controller/latest_collection_controller.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+import '../../../core/controllers/currency_controller.dart';
+import '../../../core/services/api_service.dart';
+import '../../../data/repositories/product_repository.dart';
+import '../model/product_model.dart';
+
+class LatestCollectionController extends GetxController {
+  final ProductRepository _repo = ProductRepository(ApiService());
+
+  final RxBool isLoading = false.obs;
+  final RxList<ProductModel> products = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadLatest();
+    
+    ever(Get.find<CurrencyController>().selectedCurrency, (_) {
+      products.refresh();
+    });
+  }
+
+  Future<void> loadLatest() async {
+    try {
+      isLoading.value = true;
+      final page = await _repo.fetchPaged(page: 1, perPage: 8, sorting: 'newest');
+      products.assignAll(page.items);
+    } catch (e) {
+      products.clear();
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
