092d1e2fd5dea60f57490a85fbeebf40a43cc4d5
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-23 21:01:15 +0100

    Update contact_repository.dart

--- lib/data/repositories/contact_repository.dart
+++ lib/data/repositories/contact_repository.dart
@@ -1,3 +1,6 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
+
 import '../../core/config/app_config.dart';
 import '../../core/services/api_service.dart';
 import '../../modules/account/model/contact_message_model.dart';
@@ -13,6 +16,7 @@ class ContactRepository {
     required String email,
     required String subject,
     required String message,
+    List<PlatformFile>? files,
   }) async {
     final fields = <String, String>{
       'name': name,
@@ -21,9 +25,22 @@ class ContactRepository {
       'message': message,
     };
 
+    List<http.MultipartFile>? multipartFiles;
+    if (files != null && files.isNotEmpty) {
+      multipartFiles = [];
+      for (final file in files) {
+        if (file.path != null) {
+          multipartFiles.add(
+            await http.MultipartFile.fromPath('attachments[]', file.path!),
+          );
+        }
+      }
+    }
+
     final json = await _apiService.postMultipart(
       AppConfig.storeContactMessageUrl(),
       fields: fields,
+      files: multipartFiles,
     );
 
     return ContactMessageResponse.fromJson(json);
