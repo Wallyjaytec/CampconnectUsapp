42a0c45158db89e4ce03f0a78503e26824495ecb
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-24 04:45:04 +0100

    Create pending_reviews_controller.dart

---
+++ lib/modules/account/controller/pending_reviews_controller.dart
@@ -0,0 +1,62 @@
+import 'package:get/get.dart';
+import '../../../core/services/api_service.dart';
+import '../../../data/repositories/my_order_repository.dart';
+import '../model/my_order_details_model.dart';
+
+class PendingReviewsController extends GetxController {
+  PendingReviewsController({OrderRepository? repository})
+      : _repo = repository ?? OrderRepository(ApiService());
+
+  final OrderRepository _repo;
+
+  final RxBool isLoading = false.obs;
+  final RxString error = ''.obs;
+  final RxList<OrderProductItem> products = <OrderProductItem>[].obs;
+  final Map<int, int> productOrderMap = {}; // productId -> orderId
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadPendingReviews();
+  }
+
+  Future<void> loadPendingReviews() async {
+    isLoading.value = true;
+    error.value = '';
+
+    try {
+      final orderResponse = await _repo.fetchOrders(page: 1, perPage: 100);
+
+      final List<OrderProductItem> deliveredProducts = [];
+
+      for (final order in orderResponse.data) {
+        try {
+          final detailsResponse =
+              await _repo.fetchOrderDetails(orderId: order.id);
+          final orderData = detailsResponse.data;
+
+          for (final product in orderData.products) {
+            if (product.deliveryStatus == '3') {
+              deliveredProducts.add(product);
+              productOrderMap[product.productId] = orderData.id;
+            }
+          }
+        } catch (_) {}
+      }
+
+      products.assignAll(deliveredProducts);
+    } catch (e) {
+      error.value = 'Failed to load reviews'.tr;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  int getOrderIdForProduct(int productId) {
+    return productOrderMap[productId] ?? 0;
+  }
+
+  void removeProduct(int productId) {
+    products.removeWhere((p) => p.productId == productId);
+  }
+}
