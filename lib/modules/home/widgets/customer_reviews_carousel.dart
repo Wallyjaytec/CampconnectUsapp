be4722abb81fcfb2265179cbd2d9c930e9da4520
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-24 03:15:27 +0100

    Create customer_reviews_carousel.dart

---
+++ lib/modules/home/widgets/customer_reviews_carousel.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomerReviewsCarousel extends StatefulWidget {
+  const CustomerReviewsCarousel({super.key});
+
+  @override
+  State<CustomerReviewsCarousel> createState() => _CustomerReviewsCarouselState();
+}
+
+class _CustomerReviewsCarouselState extends State<CustomerReviewsCarousel> {
+  final PageController _pageController = PageController(viewportFraction: 1.0);
+  int _currentPage = 0;
+
+  final List<String> _images = [
+    'assets/icons/review.png',
+    'assets/icons/review1.png',
+    'assets/icons/review2.png',
+    'assets/icons/review3.png',
+    'assets/icons/review4.png',
+    'assets/icons/review5.png',
+    'assets/icons/review6.png',
+    'assets/icons/review7.png',
+    'assets/icons/review8.png',
+    'assets/icons/review9.png',
+    'assets/icons/review10.png',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _startAutoPlay();
+  }
+
+  void _startAutoPlay() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted && _pageController.hasClients) {
+        if (_currentPage == _images.length - 1) {
+          _pageController.jumpToPage(0);
+          setState(() => _currentPage = 0);
+        } else {
+          _pageController.nextPage(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+          );
+        }
+        _startAutoPlay();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 12, 10, 6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 4),
+            child: Text(
+              'From Our Customers'.tr,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: SizedBox(
+              height: 150,
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+                itemCount: _images.length,
+                itemBuilder: (context, index) {
+                  return Image.asset(
+                    _images[index],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 150,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: Colors.grey[300],
+                        child: const Center(
+                          child: Icon(Icons.broken_image, size: 50, color: Colors.grey),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              _images.length,
+              (index) => AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                margin: const EdgeInsets.symmetric(horizontal: 2),
+                width: _currentPage == index ? 16 : 6,
+                height: 6,
+                decoration: BoxDecoration(
+                  color: _currentPage == index
+                      ? Theme.of(context).primaryColor
+                      : Colors.grey.withValues(alpha: 0.3),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
