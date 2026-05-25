387f4fd349fc636fbcac11a99a0cf1eb71306452
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-25 16:54:12 +0100

    Update review_detail_dialog.dart

--- lib/modules/account/widgets/review_detail_dialog.dart
+++ lib/modules/account/widgets/review_detail_dialog.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:intl/intl.dart';
-import 'package:kartly_e_commerce/core/config/app_config.dart';
 import 'package:kartly_e_commerce/core/constants/app_colors.dart';
 import 'package:kartly_e_commerce/core/routes/app_routes.dart';
 import '../../product/widgets/star_row.dart';
@@ -91,7 +90,7 @@ class ReviewDetailDialog extends StatelessWidget {
                         width: 64,
                         height: 64,
                         child: CachedNetworkImage(
-                          imageUrl: AppConfig.assetUrl(productImage),
+                          imageUrl: productImage,
                           fit: BoxFit.cover,
                           errorWidget: (_, __, ___) => Container(
                             color: Colors.grey.shade300,
@@ -166,7 +165,7 @@ class ReviewDetailDialog extends StatelessWidget {
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
                           child: CachedNetworkImage(
-                            imageUrl: AppConfig.assetUrl(img),
+                            imageUrl: img,
                             width: 80,
                             height: 80,
                             fit: BoxFit.cover,
