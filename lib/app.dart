7a345a09a0f0765a98b3f4db063acc5e2839bf41
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-26 03:31:27 +0100

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
