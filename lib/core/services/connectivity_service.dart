7d5f9611ac712591352ea17ce54b905aec964742
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-27 01:25:50 +0100

    Update connectivity_service.dart

--- lib/core/services/connectivity_service.dart
+++ lib/core/services/connectivity_service.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:kartly_e_commerce/core/utils/locale_mapper.dart';
+import 'package:kartly_e_commerce/core/services/language_service.dart';
 
 class ConnectivityService extends GetxService {
   final Connectivity _connectivity = Connectivity();
@@ -16,12 +17,18 @@ class ConnectivityService extends GetxService {
     return this;
   }
 
-  void _syncLanguageWhenOnline() {
+  void _syncLanguageWhenOnline() async {
     final savedLang = _box.read<String>('selected_language_api_code');
     if (savedLang != null && savedLang.isNotEmpty) {
-      final currentLocale = Get.locale;
+      // Re-fetch translations from API now that we're back online
+      try {
+        await LanguageService.load(savedLang, force: true);
+      } catch (_) {}
+
+      // Then update the locale
       final expectedLocale = LocaleMapper.fromApiCode(savedLang);
-      
+      final currentLocale = Get.locale;
+
       if (currentLocale?.languageCode != expectedLocale.languageCode) {
         Get.updateLocale(expectedLocale);
       }
