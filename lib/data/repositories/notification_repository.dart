11f07e1464aab223b8d4bc1eb8e4ac14fb904b73
Wally Jay Tech <wallyjay111@gmail.com>
2026-05-29 02:31:56 +0100

    Update notification_repository.dart

--- lib/data/repositories/notification_repository.dart
+++ lib/data/repositories/notification_repository.dart
@@ -6,8 +6,8 @@ class NotificationRepository {
   NotificationRepository({ApiService? api}) : _api = api ?? ApiService();
   final ApiService _api;
 
-  Future<UnreadListResponse> fetchUnreadNotifications() async {
-    final url = AppConfig.unreadNotificationsUrl();
+  Future<UnreadListResponse> fetchAllNotifications() async {
+    final url = AppConfig.allNotificationsUrl();
     final json = await _api.getJson(url);
     return UnreadListResponse.fromJson(json);
   }
@@ -19,18 +19,5 @@ class NotificationRepository {
     final body = {'id': notificationId};
     final json = await _api.postJson(url, body: body);
     return SingleMarkResponse.fromJson(json);
-  }
-
-  Future<bool> markAllAsRead() async {
-    final url = AppConfig.markAllNotificationsReadUrl();
-    final json = await _api.getJson(url);
-    final success = json['success'] == true || json['success']?.toString() == 'true';
-    return success;
-  }
-
-  Future<bool> deleteNotification(String id) async {
-    final url = AppConfig.deleteNotificationUrl();
-    final json = await _api.postJson(url, body: {'id': id});
-    return json['success'] == true;
   }
 }
