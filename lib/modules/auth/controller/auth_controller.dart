3840b828aec6e239055b2eb4a23f66eb7cff19c3
Wally Jay Tech <wallyjay111@gmail.com>
2026-06-01 18:00:05 +0100

    Update auth_controller.dart

--- lib/modules/auth/controller/auth_controller.dart
+++ lib/modules/auth/controller/auth_controller.dart
@@ -221,13 +221,13 @@ class AuthController extends GetxController {
       storage.saveLoginUser(loginRes.user);
       storage.saveDashboardContent(loginRes.dashboardContent);
       
-      await PasscodeService.checkPasscodeOnServer();
+      final hasPasscode = await PasscodeService.checkPasscodeOnServer();
       
       _showSnackbar('Success'.tr, 'Login successful'.tr);
       final args = Get.arguments is Map ? Get.arguments as Map : null;
       final redirect = args?['redirect'] as String?;
 
-      if (PasscodeService.isPasscodeEnabled()) {
+      if (hasPasscode) {
         Get.offAll(() => PasscodeLockScreen(
           onUnlocked: () {
             final box = GetStorage();
