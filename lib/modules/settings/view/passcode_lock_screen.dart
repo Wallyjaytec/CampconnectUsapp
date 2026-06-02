97b7345ebdea519fd4b95d3bd30e16c8e6d23b30
Wallyjaytec <wallyjay666@gmail.com>
2026-06-02 10:28:17 +0100

    Update passcode_lock_screen.dart

--- lib/modules/settings/view/passcode_lock_screen.dart
+++ lib/modules/settings/view/passcode_lock_screen.dart
@@ -154,7 +154,9 @@ class _PasscodeLockScreenState extends State<PasscodeLockScreen> with WidgetsBin
       _checkingPasscode = false;
       setState(() { _passcode = ''; _errorMessage = ''; _isLockedOut = false; });
       if (mounted) {
-        Navigator.of(context).popUntil((route) => route.isFirst);
+        // Pop back to lock screen (2 screens: PasscodeInputView + ForgotPasscode)
+        Navigator.of(context).pop();
+        Navigator.of(context).pop();
         isLockScreenShowing = false;
         Future.delayed(const Duration(milliseconds: 300), () {
           if (mounted) {
