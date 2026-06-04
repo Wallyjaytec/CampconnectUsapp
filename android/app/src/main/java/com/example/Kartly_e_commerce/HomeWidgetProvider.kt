d9dcc6dbfb20a9f132c0f7839e400acfbf21fb51
Wallyjaytec <wallyjay666@gmail.com>
2026-06-04 12:40:15 +0100

    Update HomeWidgetProvider.kt

--- android/app/src/main/java/com/example/Kartly_e_commerce/HomeWidgetProvider.kt
+++ android/app/src/main/java/com/example/Kartly_e_commerce/HomeWidgetProvider.kt
@@ -5,7 +5,6 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.widget.RemoteViews
 
 class HomeWidgetProvider : AppWidgetProvider() {
@@ -19,15 +18,13 @@ class HomeWidgetProvider : AppWidgetProvider() {
             val views = RemoteViews(context.packageName, R.layout.home_widget_layout)
 
             val intent = Intent(context, MainActivity::class.java).apply {
-                action = Intent.ACTION_VIEW
-                data = Uri.parse("https://campconnectus.store")
                 flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
             }
             val pending = PendingIntent.getActivity(
                 context, 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
-            views.setOnClickPendingIntent(R.id.widget_search_bar, pending)
+            views.setOnClickPendingIntent(android.R.id.content, pending)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
