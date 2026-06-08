c5b8209f1fcf45ce88732d40f7db5aa5d5e6c9a3
Wallyjaytec <wallyjay666@gmail.com>
2026-06-08 19:54:33 +0100

    Update app_routes.dart

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -42,6 +42,7 @@ class AppRoutes {
   static const followSellerView = '/follow_seller_view';
   static const reportSellerView = '/report_seller_view';
   static const contactUsView = '/contact_us_view';
+  static const requestFeatureView = '/request_feature_view';
   static const guestCheckoutView = '/guest_checkout_view';
   static const orderSummaryView = '/order_summary_view';
   static const guestOrderSummaryView = '/guest_order_summary_view';
