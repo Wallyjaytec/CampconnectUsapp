5df856895e022b1bc1fd984bee590b15d6544942
Wallyjaytec <wallyjay666@gmail.com>
2026-06-10 16:11:12 +0100

    Update app_routes.dart

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -50,6 +50,7 @@ class AppRoutes {
   static const privacyPolicyView = '/privacy_policy_view';
   static const termsConditionsView = '/terms_conditions_view';
   static const faqView = '/faq_view';
+  static const returnPolicyView = '/return_policy_view';
   static const refundRequestDetailsView = '/refund_request_details_view';
   static const followingProductsView = '/following_products_view';
   static const recentlyViewedView = '/recently_viewed_view';
